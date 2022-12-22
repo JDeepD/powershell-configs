d12750290c37408714ae26bb14473b621e85b14a
Jaydeep Das <jaydeepjd.8914@gmail.com>
2022-12-22 11:37:10 +0530

    feat: Can create custom profiles now

--- scripts/setproxy.ps1
+++ scripts/setproxy.ps1
@@ -1,32 +1,49 @@
-$unset = New-Object System.Management.Automation.Host.ChoiceDescription '&unset', ''
-
-$JsonProfiles = Get-Content -Raw proxyservers.json | ConvertFrom-Json
-
+$unset = New-Object System.Management.Automation.Host.ChoiceDescription '&Unset', ''
 [System.Management.Automation.Host.ChoiceDescription[]]$options = ($unset)
-
-forEach($profile in $JsonProfiles.profiles){
-	$options += New-Object System.Management.Automation.Host.ChoiceDescription $profile.name, ($profile.proxyserver + ":" + $profile.proxyport)
-}
+$proxyserverspath = "$HOME\OneDrive\Documents\scripts\proxyservers.json"
+$JsonProfiles = Get-Content -Raw $proxyserverspath | ConvertFrom-Json
+$argv=$args[0]
+$nicks = @{};
 
 function setproxy($proxyserver){
-		Write-Output 'Setting Proxy to' $proxyserver;
-		Write-Output 'Setting git proxy...';
-		git config --global http.proxy $proxyserver;
-		git config --global https.proxy $proxyserver;
-		Write-Output 'Done...';
-		Write-Output 'Setting npm proxy...';
-		npm config set proxy $proxyserver;
-		Write-Output 'Done...';
+	Write-Output 'Setting Proxy to' $proxyserver;
+	Write-Output 'Setting git proxy...';
+	git config --global http.proxy $proxyserver;
+	git config --global https.proxy $proxyserver;
+	Write-Output 'Done...';
+	Write-Output 'Setting npm proxy...';
+	npm config set proxy $proxyserver;
+	Write-Output 'Done...';
 }
 
 function unsetproxy{
-		Write-Output 'Unsetting git proxy vars...';
-		git config --global --unset http.proxy;
-		git config --global --unset https.proxy;
-		Write-Output 'Done...';
-		Write-Output 'Unsetting npm proxy vars...'
-		npm config rm proxy;
-		Write-Output 'Done...';
+	Write-Output 'Unsetting git proxy vars...';
+	git config --global --unset http.proxy;
+	git config --global --unset https.proxy;
+	Write-Output 'Done...';
+	Write-Output 'Unsetting npm proxy vars...'
+	npm config rm proxy;
+	Write-Output 'Done...';
+}
+
+function create{
+	$name = Read-Host "Enter a Name for Profile"
+	if ($name -in ("create", "list", "unset")){
+		Write-Output "Cannot use reserved keywords for profile names"
+		exit;
+	}
+	$nickname = Read-Host "Enter a nickname for Profile"
+	$proxyserver = Read-Host "Enter proxyserver"
+	$proxyport = Read-Host "Enter proxyport"
+	$JsonProfiles.profiles += @(@{
+		"name" = "&" + $name
+		"nickname" = $nickname
+		"proxyserver" = $proxyserver
+		"proxyport" = $proxyport
+	})
+	$JsonProfiles | ConvertTo-Json | Set-Content $proxyserverspath
+	Write-Output "Profile saved in $proxyserverspath"
+
 }
 
 function setproxyinteractive{
@@ -41,24 +58,35 @@ function setproxyinteractive{
 	}
 }
 
-$table = @{
-	"hostel" = "http://172.16.199.40:8080"
-	"library" = "http://172.16.199.20:8080"
-	"h9" = "http://172.16.199.41:8080"
+forEach($profile  in $JsonProfiles.profiles){
+	$nicks += @{$profile.nickname = ($profile.proxyserver + ":" + $profile.proxyport)}
 }
 
-$argv=$args[0]
-
-if($argv -eq "unset"){
+if($JsonProfiles.profiles.count -eq 0){
+	Write-Output "No Configs Available in $proxyserverspath"
+}
+elseif($argv -eq "unset"){
 	unsetproxy;
+	exit;
 }
-elseif($argv -in $table.Keys){
-	setproxy($table[$proxyserver])
+elseif($argv -in $nicks.Keys){
+	setproxy($nicks[$argv])
+	exit;
 }
 elseif($argv -eq "list"){
-	Write-Output $table;
+	forEach($profile in $JsonProfiles.profiles){
+		Write-Output $profile
+	}
+	exit;
+}
+elseif($argv -eq "create"){
+	create;
 }
 else{
 	Write-Output "No proxy server found. Choose from below: ";
+	forEach($profile in $JsonProfiles.profiles){
+		$options += New-Object System.Management.Automation.Host.ChoiceDescription $profile.name, ($profile.proxyserver + ":" + $profile.proxyport)
+}
 	setproxyinteractive;
+	exit;
 }
