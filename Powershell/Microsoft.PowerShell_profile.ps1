0e8144b7a8fea948c3d75e37e3fab63ef17a1a78
Jaydeep Das <jaydeepjd.8914@gmail.com>
2023-03-11 00:59:25 +0530

    Donot show errors on exit

--- Powershell/Microsoft.PowerShell_profile.ps1
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -19,27 +19,50 @@ function plugins {
 }
 
 function win10 {
-	cd $HOME/OneDrive/Documents/
+	Set-Location $HOME/OneDrive/Documents/
 }
 
 function nvc {
-	cd $env:LOCALAPPDATA/nvim/
+	Set-Location $env:LOCALAPPDATA/nvim/
 }
 
 function fvim {
-  nvim $(fzf  --pointer=' ' --border --info=inline --prompt='▶ ' --color=fg+:#000000,bg+:#FF0000,gutter:-1 --exact)
+  $file=$(fzf  --pointer=' ' --border --info=inline --prompt='▶ ' --color=fg+:#FFFFFF,bg+:#FF0000,gutter:-1 --exact --preview "bat --style=numbers --color=always --line-range :500 {}")
+  if ($file){
+    nvim $file
+  }
+}
+
+function fcp {
+  $from=$(fd | fzf  --pointer=' ' --border --info=inline --prompt='(Select file to copy) ▶ ' --color=fg+:#FFFFFF,bg+:#FF0000,gutter:-1 --exact)
+  $to=$(fd --type directory | fzf  --pointer=' ' --border --info=inline --prompt='(Select directory to paste) ▶ ' --color=fg+:#FFFFFF,bg+:#FF0000,gutter:-1 --exact)
+
+  if($from && $to){
+    Write-Host "Copying from: $from"
+    Write-Host "Pasting to: $to"
+    Copy-Item $from $to -Recurse
+    Write-Host "Done"
+  }else{
+    Write-Host "Missing from or to params..."
+  }
 }
 
 function fopen {
-	Start-Process $(fzf  --pointer=' ' --border --info=inline --prompt='▶ ' --color=fg+:#000000,bg+:#FF0000,gutter:-1 --exact)
+  $file=$(fzf  --pointer=' ' --border --info=inline --prompt='▶ ' --color=fg+:#FFFFFF,bg+:#FF0000,gutter:-1 --exact)
+  if($file){
+    Start-Process $file
+  }
 }
 
 function fcd {
-	cd (fd --type directory | fzf  --pointer=' ' --border --info=inline --prompt='▶ ' --color=fg+:#FFFFFF,bg+:#FF0000,gutter:-1 --exact)
+	Set-Location (fd --type directory | fzf  --pointer=' ' --border --info=inline --prompt='▶ ' --color=fg+:#FFFFFF,bg+:#FF0000,gutter:-1 --exact)
 }
 
 function fcode {
-	code (fd --type directory | fzf  --pointer=' ' --border --info=inline --prompt='▶ ' --color=fg+:#000000,bg+:#FF0000,gutter:-1 --exact )
+  $dir=$(fd --type directory | fzf  --pointer=' ' --border --info=inline --prompt='▶ ' --color=fg+:#FFFFFF,bg+:#FF0000,gutter:-1 --exact)
+	if($dir){
+      code $dir
+  }
 }
 
 # For zoxide v0.8.0+
